dba0c30be08ae6ff1965b87b5a2f895946b4732e
shreshthns806 <shreshthns.425@gmail.com>
2020-05-10 10:36:43 +0530

    World is our canvas part 1

--- sketch.js
+++ sketch.js
@@ -1,29 +1,50 @@
-var ball;
+var database;
+var drawing = [], currentPath=[], isDrawing = false;
 
 function setup(){
-    createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    canvas = createCanvas(400,400);
+    canvas.mousePressed(startPath);
+    canvas.parent('canvascontainer');
+    canvas.mouseReleased(endPath);
+    database = firebase.database();
+    var saveButton = select('#saveButton');
+    saveButton.mousePressed(saveDrawing);
+}
+
+function startPath(){
+    isDrawing = true;
+    currentPath=[];
+    drawing.push(currentPath)
+}
+
+function endPath(){
+    isDrawing = false;
 }
 
 function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
-}
+    background(0);
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    if(isDrawing){
+        var point = {
+            x: mouseX,
+            y: mouseY
+        }
+        currentPath.push(point);
+    }
+
+    stroke(255);
+    strokeWeight(5);
+    noFill();
+    for (var i=0; i<drawing.length; i++){
+        var path = drawing[i];
+        beginShape();
+        for (var j=0; j<path.length; j++){
+            vertex (path[j].x,path[j].y);
+        }
+        endShape();
+    }
+}
+function saveDrawing(){
+    var ref = database.ref('drawings')
+    ref.push(drawing); 
 }
